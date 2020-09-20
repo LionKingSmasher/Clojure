cd7bc3753ee38b3751a9a9d74d77d11a028b9402
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-20 18:50:59 +0900

    [수정]각도 수정
    각도 표기가 라디안 표기라는 것을 알고, 수정

--- test.clj
+++ test.clj
@@ -1,5 +1,7 @@
 (def message "TEST")
 (def g 9.8)
+(defn rad [a](* a (/ 180 Math/PI)))
+(defn r2a [a](* a (/ Math/PI 180)))
 (defn add
 	([a b](+ a b))
 )
@@ -9,13 +11,13 @@
 
 (defn CannonX
 	([V0 Angle t]
-		(def x (* t (* V0 (Math/cos (* Angle (/ 180 Math/PI))))))
+		(def x (* t (* V0 (Math/cos (r2a Angle)))))
 		(format "Result X: %f" x)
 		)
 	)
 (defn CannonY
 	([V0 Angle t]
-		(def y (- (* t (* V0 (Math/sin (* Angle (/ 180 Math/PI))))) (* g (* t t))))
+		(def y (- (* t (* V0 (Math/sin (r2a Angle)))) (* g (* t t))))
 		(format "Result Y: %f" y)
 		)
 	)
